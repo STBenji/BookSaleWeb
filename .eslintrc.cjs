7d3d52608d92e6d9792f1d5aa3c0e2deab5f585b
STBenji <stevenbenjumea9@gmail.com>
2024-05-29 16:46:10 -0500

    New eslint rules added and Next UI configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,19 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'standard'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'standard'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     semi: ['error', 'never'],
+    'comma-dangle': 0,
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import { Button } from '@nextui-org/react'
 
 function App() {
   return (
     <>
-      <h1 className="font-bold text-3xl">CBook</h1>
+      <h1 className="text-3xl font-bold">CBook</h1>
+      <Button>Hola</Button>
     </>
   )
 }
